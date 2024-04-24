174066d2f6967eaf2838b075c47e417b556a52d5
Darchevil <yderneville@gmail.com>
2024-04-24 07:20:54 +0200

    intégration tailwindcss

---
+++ parlons-bilingue/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      inter: ["inter"],
+      space: ["SpaceGrotesk"],
+      basker: ["Baskervville"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
