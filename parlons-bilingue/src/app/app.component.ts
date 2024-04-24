fa8663f061e80e63c8223d17efef9b5c77a0419f
Darchevil <yderneville@gmail.com>
2024-04-24 21:42:23 +0200

    logo + title text + css

--- parlons-bilingue/src/app/app.component.ts
+++ parlons-bilingue/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccueilSectionComponent } from './components/accueil-section/accueil-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AccueilSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
