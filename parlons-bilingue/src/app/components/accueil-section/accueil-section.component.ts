2e4e987d7abb3b74be4fed6a718e3c7b1d118a88
Darchevil <yderneville@gmail.com>
2024-04-25 22:24:12 +0200

    section accueil

--- parlons-bilingue/src/app/components/accueil-section/accueil-section.component.ts
+++ parlons-bilingue/src/app/components/accueil-section/accueil-section.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonLargeComponent } from '../shared/button-large/button-large.component';
 
 @Component({
   selector: 'app-accueil-section',
   standalone: true,
-  imports: [],
+  imports: [ButtonLargeComponent],
   templateUrl: './accueil-section.component.html',
-  styleUrl: './accueil-section.component.scss'
+  styleUrl: './accueil-section.component.scss',
 })
 export class AccueilSectionComponent {
-
+  textSavoirPlus: string = 'En savoir plus';
 }
--- parlons-bilingue/src/app/components/shared/button-large/button-large.component.ts
+++ parlons-bilingue/src/app/components/shared/button-large/button-large.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-button-large',
+  selector: 'button-large',
   standalone: true,
   imports: [],
   templateUrl: './button-large.component.html',
-  styleUrl: './button-large.component.css'
+  styleUrl: './button-large.component.css',
 })
 export class ButtonLargeComponent {
-
+  @Input() textButton: string = 'Button';
 }
