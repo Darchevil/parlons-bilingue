e311abfa5bc3cc045902dc5e6ad17d59624c80d1
Darchevil <yderneville@gmail.com>
2024-05-09 00:18:36 +0200

    new component service section

--- parlons-bilingue/src/app/app.component.ts
+++ parlons-bilingue/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AccueilSectionComponent } from './components/accueil-section/accueil-section.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { ServiceSectionComponent } from './components/service-section/service-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AccueilSectionComponent, NavigationComponent],
+  imports: [
+    RouterOutlet,
+    AccueilSectionComponent,
+    NavigationComponent,
+    ServiceSectionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
