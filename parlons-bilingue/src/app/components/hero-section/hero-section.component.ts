1df9e5a559d8772c3394bad6645fbb1b7fbe4e11
Darchevil <yderneville@gmail.com>
2024-05-11 11:03:33 +0200

    position button hero

--- parlons-bilingue/src/app/components/hero-section/hero-section.component.ts
+++ parlons-bilingue/src/app/components/hero-section/hero-section.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonLargeComponent } from '../shared/button-large/button-large.component';
 
 @Component({
   selector: 'hero-section',
   standalone: true,
-  imports: [],
+  imports: [ButtonLargeComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css',
 })
-export class HeroSectionComponent {}
+export class HeroSectionComponent {
+  text: string = 'Je me lance !';
+}
