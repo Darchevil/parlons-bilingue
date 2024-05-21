8b52a62a35d3b529e48fa4fb0f62052336c48031
Darchevil <yderneville@gmail.com>
2024-05-21 19:29:37 +0200

    création formulaire contact

--- parlons-bilingue/src/app/components/contact/contact.component.ts
+++ parlons-bilingue/src/app/components/contact/contact.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  name: FormControl = new FormControl('');
+  email: FormControl = new FormControl('');
+  subject: FormControl = new FormControl('');
+  message: FormControl = new FormControl('');
+}
