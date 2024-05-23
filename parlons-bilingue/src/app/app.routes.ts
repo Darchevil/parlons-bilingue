64d21122f7c0a8ba33bf0c95f0253c4106a2c1d8
Darchevil <yderneville@gmail.com>
2024-05-24 00:32:18 +0200

    Correction navigation + Page tarif WIP

--- parlons-bilingue/src/app/app.routes.ts
+++ parlons-bilingue/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { TarifsComponent } from './tarifs/tarifs.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [{ path: 'tarifs', component: TarifsComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tarifs', component: TarifsComponent },
+];
