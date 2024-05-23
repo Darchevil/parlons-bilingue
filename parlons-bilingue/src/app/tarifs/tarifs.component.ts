13cfa35cc2b02722601f9fe2ec8a1ce383d7b9cd
Darchevil <yderneville@gmail.com>
2024-05-24 01:56:12 +0200

    ajustements CSS

--- parlons-bilingue/src/app/tarifs/tarifs.component.ts
+++ parlons-bilingue/src/app/tarifs/tarifs.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonLargeComponent } from '../components/shared/button-large/button-large.component';
+import { NavigationComponent } from '../components/navigation/navigation.component';
 
 @Component({
   selector: 'app-tarifs',
   standalone: true,
-  imports: [ButtonLargeComponent],
+  imports: [ButtonLargeComponent, NavigationComponent],
   templateUrl: './tarifs.component.html',
   styleUrl: './tarifs.component.css',
 })
