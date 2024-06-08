48cc8838bd06017bcebc77414b2903f5b64bfea7
Darchevil <yderneville@gmail.com>
2024-06-08 21:43:16 +0200

    new build

--- parlons-bilingue/dist/parlons-bilingue/server/main.server.mjs
+++ parlons-bilingue/dist/parlons-bilingue/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-64DAJ73U.mjs";import"./chunk-AZWE2Q5K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-LV5G5DCJ.mjs";import"./chunk-AZWE2Q5K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
